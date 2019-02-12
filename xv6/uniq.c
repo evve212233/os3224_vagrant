30a77d359eeac5788dc426b1c2a5016f9483e6de
evve212233 <zz1456@nyu.edu>
2019-02-11 23:54:22 -0500

    hw1 part3
    made the code neater and more readable

--- xv6/uniq.c
+++ xv6/uniq.c
@@ -2,58 +2,127 @@
 #include "stat.h"
 #include "user.h"
 
-char buf[512];
+char prev[1024], cur[1024];
+int cFlag, dFlag, iFlag;
 
-void uniq(int fd, char *name) {
-	int i, n;
-	int l, c;
-	l = c = 0;
-	char string[512];
-	char prev[512];
-
-	while ((n = read(fd, buf, sizeof(buf))) > 0) {
-        for (i = 0; i < n; i++) {
-        	string[c] = buf[i];
-            c++;
-            if (buf[i] == '\n') {
-                string[c] = '\0';
-                prev[l] = '\0';
-                l = strlen(string);
-                if (strcmp(prev, string) !=0){
-                	write(1, string, strlen(string));
-                }
-                strcpy(prev, string);
-                memset(string, 0, 512);
-                c = 0;
-            }
-        }
-    }
-    if(n < 0){
-	    printf(1, "uniq: read error\n");
-	    exit();
-  }
-    if (strcmp(prev, string) !=0){
-        write(1, string, strlen(string));
-    }
+//for i flag, convert each the upper case to lower case to compare
+int i_sensitive(const char* prev, const char* curr){
+	const char* str1 = prev;
+	const char* str2 = curr;
+	char temp_str1;
+	char temp_str2;
+	while(*str1 != '\0'){
+		if(*str1 <= 'Z' && *str1 >= 'A'){
+			temp_str1 = *str1 + 32;
+		}else{
+			temp_str1 = *str1;
+		}
+		if(*str2 <= 'Z' && *str2 >= 'A'){
+			temp_str2 = *str1 + 32;
+		}else{
+			temp_str2 = *str2;
+		}
+		int diff = temp_str1 - temp_str2;
+		str1++;
+		str2++;
+		if(diff) return diff; 
+	}
+	if(*str2 == '\0') return 0;
+  	return 1;
 }
 
-int
-main(int argc, char *argv[])
-{
-  int fd, i;
+int readLine(int fd, char *input, int count){
+	int curr_ind;
+	char elem;
+	curr_ind = 0;
+	// if the file input is invalid, return -1
+	while(curr_ind < count){
+		if(read(fd, &elem, 1) < 1) return -1;
+		input[curr_ind] = elem;
+		curr_ind += 1;
+		if(elem == '\n') break;
+	}
+	input[curr_ind] = '\0';
+	return curr_ind;
+}
 
-  if(argc <= 1){
-    uniq(0, "");
-    exit();
-  }
+void uniq(int fd){
+	char *prev_str, *curr_str, *temp;
+	int count;
+	count = 1;
+	prev_str = prev;
+	int i_cond, d_cond;
+	if(readLine(fd,prev_str,1024) < 0) exit();
+	curr_str = cur;
+	while( readLine(fd,curr_str,1024) > 0 ){
+		i_cond = (iFlag) ? i_sensitive(prev_str, curr_str) : strcmp(prev_str,curr_str);
+		if(i_cond){
+			d_cond = count > 1 && dFlag;
+			if(!dFlag || d_cond){
+				if(cFlag){
+					printf(1, "%d %s", count, prev_str);
+				}else{
+					printf(1, "%s", prev_str);
+				}
+			}
+			temp = curr_str;
+			curr_str = prev_str;
+			prev_str = temp;
+			count = 1;
+		}else{
+			count++;
+		}
+	}
+	d_cond = count > 1 && dFlag;
+	if(d_cond || !dFlag){
+		if(cFlag){
+			printf(1, "%d %s", count, prev_str);
+		}else{
+			printf(1, "%s", prev_str);
+		}
+	}
+}
 
-  for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], 0)) < 0){
-      printf(1, "wc: cannot open %s\n", argv[i]);
-      exit();
-    }
-    uniq(fd, argv[i]);
-    close(fd);
-  }
-  exit();
+
+int main(int argc, char *argv[]) {
+	int fd, i;
+	int condC, condD, condI;
+	cFlag = 0, dFlag = 0, iFlag = 0;
+	if(argc <= 1){
+		uniq(0);
+		exit();
+	}
+
+	for(i = 1; i < argc; i++){
+		switch(argv[i][1]){
+			case 'c':
+				cFlag = 1;
+				break;
+			case 'd':
+				dFlag = 1;
+				break;
+			case 'i':
+				iFlag = 1;
+				break;
+		}
+	}
+
+	if(cFlag == 1 && dFlag == 1){
+		printf(1, "uniq: cannot open -c -d at same time\n");
+		exit();
+	}
+
+	condC = strcmp(argv[(argc-1)], "-c");
+	condD = strcmp(argv[(argc-1)], "-d");
+	condI = strcmp(argv[(argc-1)], "-i");
+
+	if(condC == 0 || condD == 0 || condI == 0){
+		uniq(0);
+	} else if ((fd = open(argv[(argc-1)], 0)) < 0){
+		printf(1, "uniq: cannot open %s\n", argv[(argc-1)]);
+		exit();
+	}
+	uniq(fd);
+	close(fd);
+	exit();
 }
---
+++ xv6/uniqtst.c
@@ -0,0 +1,63 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+void 
+uniq(int fd, char *name) {
+	int i, n;
+	int l, c;
+	l = c = 0;
+	char string[512];
+	char prev[512];
+
+	while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        for (i = 0; i < n; i++) {
+        	string[c] = buf[i];
+            c++;
+            if (buf[i] == '\n') {
+                string[c] = '\0';
+                prev[l] = '\0';
+                l = strlen(string);
+
+                if (strcmp(prev, string) != 0){
+                	write(1, string, strlen(string));
+                }
+                strcpy(prev, string);
+                memset(string, 0, 512);
+                c = 0;
+            }
+        }
+    }
+    if(n < 0){
+	    printf(1, "uniq: read error\n");
+	    exit();
+  }
+    if (strcmp(prev, string) !=0){
+        write(1, string, strlen(string));
+    }
+}
+
+
+
+int
+main(int argc, char *argv[])
+{
+  int fd, i;
+
+  if(argc <= 1){
+    uniq(0, "");
+    exit();
+  }
+
+  for(i = 1; i < argc; i++){
+    if((fd = open(argv[i], 0)) < 0){
+      printf(1, "wc: cannot open %s\n", argv[i]);
+      exit();
+    }
+    uniq(fd, argv[i]);
+    close(fd);
+  }
+  exit();
+}
